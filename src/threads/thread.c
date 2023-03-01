1baa09700908d6dfe771429ca60a7314fc4bf0b9
Shaolun Zhang <shaolun_zhang@berkeley.edu>
2023-02-28 18:20:31 -0800

    added asm(fninit) in kernel_thread function in thread.c to initialize the FPU(?)

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -416,7 +416,7 @@ static void idle(void* idle_started_ UNUSED) {
 /* Function used as the basis for a kernel thread. */
 static void kernel_thread(thread_func* function, void* aux) {
   ASSERT(function != NULL);
-
+  asm("fninit");
   intr_enable(); /* The scheduler runs with interrupts off. */
   function(aux); /* Execute the thread function. */
   thread_exit(); /* If function() returns, kill the thread. */
